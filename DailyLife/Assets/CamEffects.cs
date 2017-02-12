893954098c65c2879db56d5e7f028026ca220486
BaileyFit <1005703@student.sae.edu.au>
2017-02-12 16:49:32 +1000

    Added gray scale to show happiness, some more choices, futher along in story.

---
+++ DailyLife/Assets/CamEffects.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+using UnityStandardAssets.ImageEffects;
+
+public class CamEffects : MonoBehaviour {
+
+    public Flowchart flowchart;
+    public float unhappiness;
+
+
+   
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        unhappiness = flowchart.GetFloatVariable("Unhappiness");
+        GetComponent<Grayscale>().effectAmount = unhappiness;
+	}
+}
