029c45f415ce3149d98921c9dcc22a7e03cdbba1
Bailey Fitness <bfitness@sae.au>
2017-02-15 20:21:12 +1000

    Playable

---
+++ DailyLife/Assets/Quit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class Quit : MonoBehaviour {
+
+    public Flowchart flowchart;
+    public bool quit;
+
+	// Use this for initialization
+	void Start () {
+        quit = flowchart.GetBooleanVariable("Quit");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if( quit == true)
+        {
+            Application.Quit();
+        }
+	}
+}
