4391dde1819aeb0e4f62e8c26c0003c228c0ad2a
Bailey Fitness <bfitness@sae.au>
2017-02-15 17:32:58 +1000

    Playable loop

--- DailyLife/Assets/CamEffects.cs
+++ DailyLife/Assets/CamEffects.cs
@@ -8,18 +8,25 @@ public class CamEffects : MonoBehaviour {
 
     public Flowchart flowchart;
     public float unhappiness;
+    AudioSource audio;
+    public float audiopitch = 1;
 
 
-   
 
-	// Use this for initialization
-	void Start () {
-        
+    // Use this for initialization
+    void Start () {
+        audio = GetComponent<AudioSource>();
     }
 	
 	// Update is called once per frame
 	void Update () {
         unhappiness = flowchart.GetFloatVariable("Unhappiness");
+        if (unhappiness > 1)
+        {
+            unhappiness = 1;
+        }
         GetComponent<Grayscale>().effectAmount = unhappiness;
+        audio.pitch = audiopitch - unhappiness/8;
+
 	}
 }
